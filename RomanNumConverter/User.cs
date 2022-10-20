fdbc22af133ea050a0e06d89416469191fa7cce7
Victoria Nikiforova <nkfrvvv@mail.ru>
2022-10-20 21:31:31 +0400

    New branch commit

---
+++ RomanNumConverter/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumConverter
+{
+    /// <summary>
+    /// Test class for commit
+    /// </summary>
+    internal class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int UserId { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
